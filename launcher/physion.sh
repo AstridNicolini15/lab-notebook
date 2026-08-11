16c4d3b42424ee5bb5a550d4365ddf527a01436b
Astrid Nicolini <astrid.nicolini@hotmail.com>
2026-08-11 16:25:46 +0200

    add launcher

---
+++ launcher/physion.sh
@@ -0,0 +1,3 @@
+export QT_QPA_PLATFORM=xcb
+cd ~/lab-notebook/astrid/physion/src
+~/miniforge3/envs/suite2p/bin/python -m physion 
---
+++ launcher/suite2p.sh
@@ -0,0 +1,3 @@
+export QT_QPA_PLATFORM=xcb
+cd ~/lab-notebook/astrid/suite2p/src
+~/miniforge3/envs/suite2p/bin/python -m suite2p
